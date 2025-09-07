f17a4028fd824c9b1c84cdcae2c5049203fd1629
Micko Lapuz <mickolapuz19@gmail.com>
2025-09-08 02:21:39 +0800

    Created Initial Models and Entities

---
+++ FinanceTracker/FinanceTracker.Entities/Models/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceTracker.Entities.Models
+{
+    [Table("TblCategory")]
+    public class Categories
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }  
+        public string Type { get; set; } 
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ FinanceTracker/FinanceTracker.Entities/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceTracker.Entities.Models
+{
+    [Table("TblUsers")]
+    public class Users
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
